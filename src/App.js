8e29333de0fa8dfc0b392750e011f550634288e3
Dima <zelinsky.dmytro.w@gmail.com>
2021-10-24 21:35:43 +0300

    Optimized fractal display

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import {complex, multiply, add, re, im, abs} from 'mathjs'
 
 
 function App() {
-  const width = 1450
-  const height = 1000
+  const width = 1700
+  const height = 1500
   const allPoints = []
-  const diameter = 900
+  const diameter = 750
   const multiplier = diameter/4
   const setup = (p5, canvasParentRef) => {
     p5.createCanvas(width, height).parent(canvasParentRef)
+    p5.pixelDensity(1)
     for(let i = 0; i < diameter; i += 1) {
       for(let j = 0; j < diameter; j += 1) {
         let z = 0
@@ -18,7 +19,7 @@ function App() {
         if(mandelbrotFunc(0, c).abs > 2) continue
         let res
         let colorNum = 0
-        for(let i = 0; i <= 15; i++) {
+        for(let i = 0; i <= 17; i++) {
           res = mandelbrotFunc(z, c)
           if(res.abs > 2) break
           z = res.c
@@ -28,14 +29,15 @@ function App() {
         if (abs >= 2 ) {
           let point = {
             complex: c,
-            r: re(c),
-            i: im(c),
+            re: re(c),
+            im: im(c),
             color: colorNum
           }
           allPoints.push(point)
         }
       }
     }
+    console.log(allPoints)
   }
   //z = z^2 + C
   const mandelbrotFunc = (z, c) => {
@@ -49,20 +51,21 @@ function App() {
   const draw = p5 => {
     p5.translate(width/2, height/2); 
     p5.scale(1, -1);
-    p5.fill(255,255,255)
-    p5.stroke(4)
-    p5.circle(0,0,diameter)
-    p5.strokeWeight(1);
+    p5.strokeWeight(3);
     p5.stroke('rgb(123,0,0)'); 
+    p5.circle(0, 0, diameter)
+    p5.loadPixels()
+    for(let i = 0; i < allPoints.length; i++) {
+      const x = allPoints[i].re * multiplier
+      const y = allPoints[i].im * multiplier
+      let index = (x + width/2 + (y + height/2) * width) * 4;
 
-    let tmp = 0
-    for(let j = 0; j < allPoints.length; j++) {
-      const r = allPoints[j].r * multiplier
-      const i = allPoints[j].i * multiplier
-      p5.stroke('rgb(' + allPoints[j].color % 255 + ','+ allPoints[j].color % 255 + ',' + allPoints[j].color % 255 +')'); 
-      p5.point(r, i)
+      p5.pixels[index + 0] = allPoints[i].color % 255;
+      p5.pixels[index + 1] = allPoints[i].color % 255;
+      p5.pixels[index + 2] = allPoints[i].color % 255;
+      p5.pixels[index + 3] = 255;
     } 
-    p5.circle(0, 0, 2)
+    p5.updatePixels()
   }
   
   return <Sketch setup={setup} draw={draw} />
