2b6c9a089d041c3862c2de3e2ee40711fada9a85
Dmytro <zelinsky.dmytro.w@gmail.com>
2021-11-16 00:19:58 +0200

    Finish color window

--- src/ColorWindow/Color.js
+++ src/ColorWindow/Color.js
@@ -1,7 +1,31 @@
-import React from 'react'
+import React , { useState, useImperativeHandle, forwardRef} from 'react'
 import Sketch from "react-p5";
+const Color = forwardRef((props, ref) => {
 
-const Color = (props) => {
+    const hsl2rgb = (h, s, l) => {
+        let r, g, b;
+      
+        if (s == 0) {
+          r = g = b = l;
+        } else {
+          function hue2rgb(p, q, t) {
+            if (t < 0) t += 1;
+            if (t > 1) t -= 1;
+            if (t < 1/6) return p + (q - p) * 6 * t;
+            if (t < 1/2) return q;
+            if (t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+            return p;
+          }
+      
+          let q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+          let p = 2 * l - q;
+      
+          r = hue2rgb(p, q, h + 1/3);
+          g = hue2rgb(p, q, h);
+          b = hue2rgb(p, q, h - 1/3);
+        }
+        return { r: r * 255, g: g * 255, b: b * 255 };
+    }
 
     const rgb2cmyk = (r, g, b, normalized) => {
         var c = 1 - (r / 255);
@@ -26,10 +50,10 @@ const Color = (props) => {
         k = isNaN(k) ? 0 : k;
         
         return {
-            c: c,
-            m: m,
-            y: y,
-            k: k,
+            c: +c.toFixed(4),
+            m: +m.toFixed(4),
+            y: +y.toFixed(4),
+            k: +k.toFixed(4),
         }
     }
 
@@ -60,33 +84,73 @@ const Color = (props) => {
     }
 
     let image 
+    const [orgPixels, setOriginalPixels] = useState([])
+    const [pointedPixel, setPointedPixel] = useState('')
+    const [p52, setP52] = useState()
+    useImperativeHandle(ref, () => ({
+        save() {
+          p52.saveCanvas()
+        }
+    }));
+
     const preload = (p5) => {
-        // p5.image(image, 0, 0, props.size.x, props.size.y)
-    };
+        image = p5.loadImage(props.imageUrl)
+    }; 
 
     const setup = (p5) => {
 		p5.createCanvas(props.size.x, props.size.y).parent('changedImage');
-        image = p5.loadImage(props.imageUrl)
-        console.log(image)
-	};
-
+        p5.pixelDensity(1)
+        p5.image(image, 0, 0, props.size.x, props.size.y)
+        p5.loadPixels()
+        for (let x = 0; x < props.size.x; x++) {
+            for (let y = 0; y < props.size.y; y++) {
+                const index = Math.round((x  + (y) * props.size.x ) * 4);
+                let hslPixel = rgb2hsl(p5.pixels[index + 0], p5.pixels[index + 1], p5.pixels[index + 2])
+                if (hslPixel.s < 0.2 && hslPixel.l < 0.60 && hslPixel.l > 0.40) {
+                    orgPixels.push({index, hslPixel})
+                }
+            }
+        }
+    };
+    
 	const draw = (p5) => {
         if(image !== undefined)
             p5.image(image, 0, 0, props.size.x, props.size.y)
-        let pixel = p5.get(p5.mouseX, p5.mouseY)
-        console.log(rgb2hsl(pixel[0],pixel[1],pixel[2]))
-        p5.fill(pixel);
-        p5.noStroke();
-        p5.rect(25, 25, 50, 50);
-	};
+        p5.loadPixels()
+        for (let x = 0; x < orgPixels.length; x++) {
+            let l
+            if (props.range < 50) {
+                l = p5.map(props.range, 0, 50, 0, orgPixels[x].hslPixel.l)
+            }
+            else {
+                l = p5.map(props.range, 50, 100, orgPixels[x].hslPixel.l, 1)
+            }
+            let changedRgbPixel = hsl2rgb(orgPixels[x].hslPixel.h, orgPixels[x].hslPixel.s, l)
+            p5.pixels[orgPixels[x].index + 0] = changedRgbPixel.r
+            p5.pixels[orgPixels[x].index + 1] = changedRgbPixel.g
+            p5.pixels[orgPixels[x].index + 2] = changedRgbPixel.b
+        }            
+        p5.updatePixels();
+        setP52(p5)
+        let rgbPixel = p5.get(p5.mouseX, p5.mouseY); 
+        if(props.colorType === 'HSL') {
+            let hslPixel = rgb2hsl(rgbPixel[0], rgbPixel[1], rgbPixel[2]) 
+            setPointedPixel('H: ' + hslPixel.h + ' S: ' + hslPixel.s + ' L: ' + hslPixel.l )
+
+        } else {
+            let cmykPixel = rgb2cmyk(rgbPixel[0], rgbPixel[1], rgbPixel[2], true) 
+            setPointedPixel('C: ' + cmykPixel.c + ' M: ' + cmykPixel.m + ' Y: ' + cmykPixel.y + ' K: ' + cmykPixel.k)
+        }
+    };
 
     return(
         <>
-            <Sketch setup={setup} draw={draw} />
+            <Sketch setup={setup} draw={draw} preload={preload}/>
             <div id='changedImage'></div>
+            <div>{pointedPixel}</div>
         </>
     )
     
-}
+})
 
 export default Color
--- src/ColorWindow/ColorWindow.js
+++ src/ColorWindow/ColorWindow.js
@@ -1,24 +1,57 @@
-import React, { useState } from "react";
-import {Col, Row, Container, Image} from 'react-bootstrap'
+import React, { useState, useRef } from "react";
+import {Col, Row, Container, Image, OverlayTrigger, Tooltip, Form} from 'react-bootstrap'
 import Color from "./Color";
-
+import leftarrow from '../Theory/TheoryNav/leftarrow.png'
+import defaultImage from '../ColorWindow/Alvin_and_the_Chipmunks.jpg'
+import Button from '@restart/ui/esm/Button';
+import { useHistory } from 'react-router-dom'
 import "./style.css";
 
 function ColorWindow() {
-    const [imageUrl, setImageUrl] = useState()
-    const imageWidth = 300
-    const imageHeight = 300
+    const history = useHistory()
+    const [imageUrl, setImageUrl] = useState(defaultImage)
+    const [range, setRange] = useState()
+    const [colorType, setColorType] = useState()
+    const colorRef = useRef()
+    const imageWidth = 400
+    const imageHeight = 400
+
   return (
     <div>
         <Container fluid >
             <Row>
                 <Col lg={4} style={{position:'relative'}}>
-                    <input type={'file'} onChange={e => setImageUrl(URL.createObjectURL(e.target.files[0]))}/>
-                  </Col>
-                  <Col lg={8}>
-                      <Color imageUrl={imageUrl} size={{x:imageWidth, y:imageWidth}} key={imageUrl}></Color>
-                      <Image src={imageUrl} width={imageWidth} height={imageHeight} ></Image>
-                  </Col>
+                    <Button className='backBtn' onClick={() => history.push('/')}>
+                      <Image src={leftarrow} fluid>
+                      </Image>
+                    </Button>
+                    <OverlayTrigger
+                        placement='right'
+                        overlay={<Tooltip> CMYK - субтрактивна модель, вона використовується для підготовки не екранних, а друкованих зображень, тобто для пристроїв, які реалізують принцип поглинання (віднімання) кольорів. Для зміни світлості зображення використовуйте повзунок "Світлота по сірому". Розширенням колірної моделі HSV/HSB є HLS. Параметрами якої є Hue, Lightness, Saturation, відповідно: кольоровий тон, кількість світла(освітленість), насиченість. </Tooltip>}>
+                        <Button className='helpBtn' onClick={() => history.push('/theory/color')}><h2><b>?</b></h2>
+                        </Button>
+                    </OverlayTrigger>
+                    <h1>Колірні моделі: CMYK та HSL</h1>
+                    <h4>Світлота по сірому кольору</h4>
+                    <Form.Range className='greyRangeSlider' min={0} max={100} step={2} defaultValue={0} onChange={(e) => setRange(e.target.value)}/>
+                    <Form.Check className={'radioBtn'} name={'group-1'}type={'radio'} onChange={() => setColorType('HSL')} label={<h4>HSL</h4>}/>
+                    <Form.Check className={'radioBtn'} name={'group-1'}type={'radio'} onChange={() => setColorType('CMYK')} label={<h4>CMYK</h4>}/>
+
+                    <Form.Control size='lg' className='modeBtn' type='file'onChange={e => setImageUrl(URL.createObjectURL(e.target.files[0]))}/>
+                    <Button className='modeBtn' onClick={() => colorRef.current.save()}>
+                        Завантажити картинку
+                    </Button>
+                </Col>
+                <Col lg={8}>
+                    <Row>
+                      <Col>
+                          <Color ref={colorRef} imageUrl={imageUrl} size={{x:imageWidth, y:imageWidth}} key={imageUrl} range={range} colorType={colorType}></Color>
+                      </Col>
+                      <Col>
+                          <Image src={imageUrl} width={imageWidth} height={imageHeight} ></Image>
+                      </Col>
+                    </Row>
+                </Col>
             </Row>
         </Container>
     </div>
